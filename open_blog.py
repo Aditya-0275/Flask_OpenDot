19e1b133b8cab89e368126e1d80654f3846f4de5
Aditya Sharma <adityasd7502@gmail.com>
2026-03-21 15:28:48 +0530

    Add templates, sample posts data, /about route

--- open_blog.py
+++ open_blog.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-print("Hello, World!")
+posts = [
+	{
+		"author":"Aditya Sharma",
+		"title":"The First Dot",
+		"date_posted":"March 21, 2026",
+		"content":"This marks the beginning of this project."
+	},
+	{
+		"author":"Mimansha Agarwal",
+		"title":"The Second Dot",
+		"date_posted":"March 21, 2026",
+		"content":"This is a dummy content for testing purposes."
+	}
+]
 
 @app.route("/")
 def home():
-	return "<h1>Home Page</h1>", 200
+	return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+	return render_template("about.html", title="About")
 
 if __name__ == '__main__':
-	app.run(debug=true)
+	app.run(debug=True)
